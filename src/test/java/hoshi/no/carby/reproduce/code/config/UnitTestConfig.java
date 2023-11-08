e0b106c86f1cd3cb5a2481adb4c996e9faa1b1cc
GENKI FURUKAWA <genki.furukawa@gmail.com>
2023-11-09 00:30:54 +0900

    [add] DIコンテナを複数生成するとtoo many clientsで落ちる再現コードを追加

--- src/test/java/hoshi/no/carby/reproduce/code/config/UnitTestConfig.java
+++ src/test/java/hoshi/no/carby/reproduce/code/config/UnitTestConfig.java
@@ -1,2 +1,23 @@
-package hoshi.no.carby.reproduce.code.config;public class UnitTestConfig {
+package hoshi.no.carby.reproduce.code.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class UnitTestConfig {
+    @Bean
+    DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres");
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setAutoCommit(false);
+        dataSource.setMaximumPoolSize(25);
+        dataSource.setMinimumIdle(25);
+        return dataSource;
+    }
 }
--- src/test/java/hoshi/no/carby/reproduce/code/service/SampleServiceTest.java
+++ src/test/java/hoshi/no/carby/reproduce/code/service/SampleServiceTest.java
@@ -1,22 +1,86 @@
 package hoshi.no.carby.reproduce.code.service;
 
-//import hoshi.no.carby.reproduce.code.config.UnitTestAppConfig;
-import org.junit.jupiter.api.Test;
+import com.zaxxer.hikari.HikariDataSource;
+import hoshi.no.carby.reproduce.code.config.UnitTestConfig;
+
+import org.junit.jupiter.api.*;
+import org.postgresql.util.PSQLException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
+
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-//@ContextConfiguration(classes = UnitTestAppConfig.class)
 @Transactional
 @DirtiesContext
 class SampleServiceTest {
+    @Autowired
+    HikariDataSource dataSource;
 
     @Test
-    void サンプル() {
+    void 明示的にDIコンテナを作成するとDBの接続数が設定値の数だけ増え_PSQLExceptionがスローされること() throws SQLException, InterruptedException {
+        try (Connection conn = dataSource.getConnection()) {
+            int numbackends = getNumbackends(conn);
+            System.out.println("（ループ0回目）コネクション数: " + numbackends);
+        }
 
+        assertThrows(PSQLException.class, () -> {
+            for (int i = 0; i < 10; i++) {
+                Thread.sleep(3_000); // NOTE: なぜか3秒待つとループの2回目から設定値の数だけ増えるようになる
+                var applicationContext = new AnnotationConfigApplicationContext(UnitTestConfig.class);
+                HikariDataSource ds = applicationContext.getBean("dataSource", HikariDataSource.class);
+                try (Connection conn = ds.getConnection()) {
+                    int numbackends = getNumbackends(conn);
+                    System.out.println("（ループ" + (i + 1) + "回目）コネクション数: " + numbackends);
+                }
+            }
+        });
+    }
+
+    @Test
+    void 明示的にDIコンテナを作成して明示的に閉じるとDBの接続数が設定値は増えないこと() throws SQLException, InterruptedException {
+        try (Connection conn = dataSource.getConnection()) {
+            int numbackends = getNumbackends(conn);
+            System.out.println("（ループ0回目）コネクション数: " + numbackends);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            var applicationContext = new AnnotationConfigApplicationContext(UnitTestConfig.class);
+            HikariDataSource ds = applicationContext.getBean("dataSource", HikariDataSource.class);
+            try (Connection conn = ds.getConnection()) {
+                int numbackends = getNumbackends(conn);
+                System.out.println("（ループ" + (i + 1) + "回目）コネクション数: " + numbackends);
+            }
+            applicationContext.close();
+        }
+    }
+
+    /**
+     * pg_stat_databaseのdatnameがpostgresの接続数を取得する
+     *
+     * @param conn conn
+     * @return 接続数
+     * @throws SQLException
+     */
+    private int getNumbackends(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        String sql = "SELECT numbackends FROM pg_stat_database where datname = 'postgres';";
+        ResultSet rs = stmt.executeQuery(sql);
+
+        while (rs.next()) {
+            int numbackends = rs.getInt("numbackends");
+            return numbackends;
+        }
+
+        return 0;
     }
 }
